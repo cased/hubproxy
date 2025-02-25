c5f5c67c800c1b74220297ce972136e9f321fd11
Joshua Peek <josh@users.noreply.github.com>
2025-02-24 18:48:13 -0800

    Safer cast

--- internal/security/tailscale_funnel_ip.go
+++ internal/security/tailscale_funnel_ip.go
@@ -8,18 +8,25 @@ import (
 	"tailscale.com/ipn"
 )
 
+type contextKey string
+
+const (
+	connectionContextKey contextKey = "connection"
+)
+
 // Hack to set http.Request.RemoteAddr to the client's IP address
 //
 // See Tailscale snippet for reference:
 // <https://github.com/tailscale/tailscale/blob/8d7033f/cmd/tsidp/tsidp.go#L1040-L1059>
 func TailscaleFunnelIP(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		netConn := r.Context().Value("connection").(net.Conn)
-		if tlsConn, ok := netConn.(*tls.Conn); ok {
-			netConn = tlsConn.NetConn()
-		}
-		if fc, ok := netConn.(*ipn.FunnelConn); ok {
-			r.RemoteAddr = fc.Src.String()
+		if netConn, ok := r.Context().Value(connectionContextKey).(net.Conn); ok {
+			if tlsConn, ok := netConn.(*tls.Conn); ok {
+				netConn = tlsConn.NetConn()
+			}
+			if fc, ok := netConn.(*ipn.FunnelConn); ok {
+				r.RemoteAddr = fc.Src.String()
+			}
 		}
 		h.ServeHTTP(w, r)
 	}
