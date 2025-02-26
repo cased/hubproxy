a1272d8747478640cccba62d4c6eb004d0fec168
Joshua Peek <josh@users.noreply.github.com>
2025-02-26 08:26:34 -0800

    Tweak bucket sizes

--- internal/metrics/middleware.go
+++ internal/metrics/middleware.go
@@ -16,7 +16,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "hubproxy_http_request_duration_seconds",
 			Help:    "Duration of HTTP requests in seconds",
-			Buckets: []float64{0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 30, 60},
+			Buckets: []float64{0.0001, 0.0005, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 30},
 		},
 		[]string{"method", "path", "status", "handler"},
 	)
